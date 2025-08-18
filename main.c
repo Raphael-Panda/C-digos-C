0b9642e74cfcb35767404464fdc9b7d2e682ff13
Raphael <74225095+Raphael-Panda@users.noreply.github.com>
2025-08-18 10:22:30 -0300

    if - else com multiplo e impar

--- main.c
+++ main.c
@@ -1,39 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+int main()
+{
+    // SENHA NUMÉRICA (XXXX) FORTE OU FRACA
+    // MULTIPLO DE 17 E ÍMPAR
+    int senha;
 
-//Escrever um algoritmo em C que leia de uma (1) mercadoria:
+    printf("Entre com sua senha de 4 números(XXXX): "); scanf("%i", &senha);
 
-//Preço: R$
-//Quantidade: Unidade
-//Reajuste (Desconto): %
-//exbir na tela:
 
-//Total a pagar: sem desconto
-//Total a pagar: com desconto
-//Total de Desconto
-
-int main() {
-
-    // ENTRADA:
-    float reajuste, preco;
-    float totalBruto, totalLiquido, desconto;
-    int quantidade;
-    printf("MERCADORIA: Dados de Entrada\n");
-    printf("Preço (R$):  "); scanf("%f", &preco);
-    printf("Quantidade (Unidade): "); scanf("%i", &quantidade);
-    printf("Reajuste (%%): "); scanf("%f", &reajuste);
-
-    // PROCESSAMENTO:
-    totalBruto = preco * quantidade;
-    desconto = reajuste/100 * totalBruto; // totalBruto aparece pois é dele que será retirado o valor do desconto
-    totalLiquido = totalBruto - desconto;
-
-    // SAÍDA:
-    printf("Dados de Pagamento: \n");
-    printf("Total (sem desconto): R$ %.2f\n", totalBruto);
-    printf("Total (com desconto: %.2f %%): R$ %.2f\n", reajuste, totalLiquido);
-    printf("Total de desconto %.2f %%: Valor do desconto R$ %.2f\n", reajuste, desconto);
-
+    if(senha <= 999 || senha >= 10000)     // OR - ||
+        printf("SENHA INCORRETA NAO POSSUI 4 DIGITOS");
+    else{
+        if(senha % 2 != 0 && senha % 17 == 0)     // AND - &&
+            printf("SUA SENHA É FORTE");
+        else
+            printf("SUA SENHA É FRACA");
+    }
     return 0;
 }
