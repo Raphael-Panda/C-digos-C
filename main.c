3c846dd809a70554c45fb749a788d0ff0598368a
Raphael <74225095+Raphael-Panda@users.noreply.github.com>
2025-09-05 08:41:41 -0300

    Do-While

--- main.c
+++ main.c
@@ -1,48 +1,53 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 
 int main()
 {
-    // ENTRADA
-    double B1, B2, PF, media;
+   int opcao, voto;
+   int moradores = 0;
+   int fulano = 0, beltrano = 0, cicrano = 0;
+   float pfulano, pbeltrano, pcicrano;
 
-    printf("Nota [0.0, 10.0] Bimestre 1? \n"); scanf("%lf", &B1); // double = Long Float  = lf
-    printf("Nota [0.0, 10.0] Bimestre 2? \n"); scanf("%lf", &B2); // double = Long Float  = lf
+   printf("MENU\n");
+   printf("[1] Cadastrar seu voto\n");
+   printf("[0] Sair do programa\n");
 
-    // TRATAMENTO DE ERRO
-    if(B1 < 0 || B1 > 10 || B2 < 0 || B2 > 10)
-        printf("ERRO: Insira as notas corretas\n");
-    else{
-        media = (B1 + B2) / 2;
-        // ARREDONDAMENTO (ROUND): 1 CASA DECIMAL
-        media = round(media * 10) / 10.0;
-        printf("MEDIA: %.1f\n", media);
-        if(media < 3.0)
-            printf("REPROVADO\n");
-        else if(media < 7.0){
-            printf("PROVA FINAL\n");
-            printf("NOTA [0.0, 10.0] DA PROVA FINAL: \n"); scanf("%lf", &PF); // double = Long Float  = lf
+    do{
+       printf("Sua Opcao: "); scanf("%i", &opcao);
+       if(opcao == 0)
+          printf("Fim do Programa\n");
+       else if(opcao > 1)
+          printf("ERRO: opcao Incorreta\n");
+       else{
+          printf("MENU: Opcao de Candidatos\n");
+          printf("1: para votar em Fulano\n");
+          printf("2: para votar em Beltrano\n");
+          printf("3: para votar em Cicrano\n");
+          do{
+            printf("Morador %i - Seu Voto: ", moradores + 1);
+            scanf("%i", &voto);
+
             // TRATAMENTO DE ERRO
-            if(PF < 0.0 || PF > 10.0)
-                printf("ERRO: Nota Incorreta\n");
+            if(voto < 1 || voto > 3)
+                printf("ERRO: opcao de voto incorreto\n");
             else{
-                media = (media + PF) / 2;
-                // ARREDONDAMENTO (ROUND): 1 CASA DECIMAL
-                media = round(media * 10) / 10.0;
-                printf("MEDIA: %.1f\n", media);
-                if(media < 5.0)
-                    printf("REPROVADO\n");
+                if(voto == 1)
+                    fulano++;
+                else if(voto == 2)
+                    beltrano++;
                 else
-                    printf("APROVADO\n");
-            }
-        }
-     else
-        printf("APROVADO\n");
-    }
-
-
-
-
+                    cicrano++;
+                moradores++;
+                printf("Resultado de Votacao;\n");
+                pfulano = 100.0 * fulano/ moradores;
+                pbeltrano = 100.0 * beltrano/ moradores;
+                pcicrano = 100.0 * cicrano/ moradores;
+                printf("Votos de Fulano: %i (%.1f %%)\n", fulano, pfulano);
+                printf("Votos de Beltrano: %i (%.1f %%)\n", beltrano, pbeltrano);
+                printf("Votos de Cicrano: %i (%.1f %%)\n", cicrano, pcicrano);
+          }
+        }while(voto < 1 || voto > 3);
+      }
+    }while(opcao != 0);
     return 0;
 }
