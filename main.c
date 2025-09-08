c38d2959c787a265cfa9e70b32573d80f96b81d8
Raphael <74225095+Raphael-Panda@users.noreply.github.com>
2025-09-08 10:47:44 -0300

    VETORES

--- main.c
+++ main.c
@@ -1,53 +1,70 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define TAMANHO 5   // Estática: Alunos
+
 int main()
 {
-   int opcao, voto;
-   int moradores = 0;
-   int fulano = 0, beltrano = 0, cicrano = 0;
-   float pfulano, pbeltrano, pcicrano;
+    int matricula[TAMANHO];             // colocar o TAMANHO para alocar os 50 espaços de memória
+    float bimestre1[TAMANHO], media;           // colocar o TAMANHO para alocar os 50 espaços de memória
+    int indice = 0, i;                      // O indice sempre começa no 0 e vai até o TAMANHO - 1
+    int opcao, soma = 0;
 
-   printf("MENU\n");
-   printf("[1] Cadastrar seu voto\n");
-   printf("[0] Sair do programa\n");
+
+    printf("MENU DE OPCOES:\n");
+    printf("1: Cadastrar Aluno\n");
+    printf("2: RELATORIO 1: Exibir Media do 1 Bimestre\n");
+    printf("3: RELATORIO 2: Pesquisar (Pesquisa Sequencial) / Exibir os Dados de um Aluno por sua Matrícula (XXX).\n");
+    printf("4: RELATORIO 3: Exibir os dados ordenados por Matrícula.\n");
+    printf("5: RELATORIO 4: Pesquisar (Binaria) por Matrícula (XXX).\n");
+    printf("0: Sair do Programa");
 
     do{
-       printf("Sua Opcao: "); scanf("%i", &opcao);
-       if(opcao == 0)
-          printf("Fim do Programa\n");
-       else if(opcao > 1)
-          printf("ERRO: opcao Incorreta\n");
-       else{
-          printf("MENU: Opcao de Candidatos\n");
-          printf("1: para votar em Fulano\n");
-          printf("2: para votar em Beltrano\n");
-          printf("3: para votar em Cicrano\n");
-          do{
-            printf("Morador %i - Seu Voto: ", moradores + 1);
-            scanf("%i", &voto);
-
-            // TRATAMENTO DE ERRO
-            if(voto < 1 || voto > 3)
-                printf("ERRO: opcao de voto incorreto\n");
-            else{
-                if(voto == 1)
-                    fulano++;
-                else if(voto == 2)
-                    beltrano++;
+        printf("Sua opcao: "); scanf("%i", &opcao);
+        if(opcao == 0)
+            printf("Fim do Programa\n");
+        else if(opcao < 0 || opcao > 5)
+            printf("ERRO: Opcao Incorreta. Escolha Novamente\n");
+        else if(opcao == 1){
+            // Cadastrar os dados: Aluno
+            if(indice < TAMANHO){
+                printf("Dados do Aluno %i: \n", indice + 1);  // Enumerar
+                printf("Matricula (XXX): "); scanf("%i", &matricula[indice]);
+                printf("Bimestre 1(Nota[0.0, 10.0]): "); scanf("%f", &bimestre1[indice]);
+                // TRATAMENTO DE ERRO:
+                if(matricula[indice] < 100 || matricula[indice] > 999)
+                    printf("ERRO: matricula incorreta. escolha novamente.\n");
+                else if(bimestre1[indice] < 0.0 || bimestre1[indice] > 10.0)
+                    printf("ERRO: Nota incorreta. escolha novamente.\n");
                 else
-                    cicrano++;
-                moradores++;
-                printf("Resultado de Votacao;\n");
-                pfulano = 100.0 * fulano/ moradores;
-                pbeltrano = 100.0 * beltrano/ moradores;
-                pcicrano = 100.0 * cicrano/ moradores;
-                printf("Votos de Fulano: %i (%.1f %%)\n", fulano, pfulano);
-                printf("Votos de Beltrano: %i (%.1f %%)\n", beltrano, pbeltrano);
-                printf("Votos de Cicrano: %i (%.1f %%)\n", cicrano, pcicrano);
-          }
-        }while(voto < 1 || voto > 3);
-      }
-    }while(opcao != 0);
+                    indice++;   // Step: indice = indice + 1
+             }
+            else
+                printf("Turma Lotada! Nao eh possivel cadastrar alunos.\n");
+        }
+        else if(opcao == 2){
+            if(indice == 0)
+                printf("Turma Vazia\n");
+            else{
+                printf("RELATORIO 1: Exibir Media do 1 Bimestre\n");
+                for( i = 0; i < indice; i++)
+                    soma += bimestre1[i];
+                media = soma/indice;
+                printf("Media do Bimestre 1: %.1f\n", media);
+            }
+        }
+        else if(opcao == 3)
+            printf("RELATORIO 2:\n");
+        else if(opcao == 4)
+            printf("RELATORIO 3:\n");
+        else
+            printf("RELATORIO 4:\n");
+
+
+
+    }while(opcao != 0);    // para continuar o programa tem q ir o contrário do tratamento de erro
+                           // justamente para o programa continuar a funciona enquanto for TRUE
+
+
     return 0;
 }
